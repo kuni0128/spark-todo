07ac53f7a3556fdce681b3144f99d8afcef56d01
Kuniaki Ishikawa <k.ishikawa.0128@gmail.com>
2020-02-23 15:57:26 +0900

    add task create method

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,15 +2,17 @@ package todolist
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import todolist.controllers.TaskController
 import spark.Spark.get
+import spark.Spark.post
+import todolist.controllers.TaskController
 import todolist.repositories.TaskRepository
 
 fun main(args: Array<String>) {
     val objectMapper = ObjectMapper().registerKotlinModule()
     val jsonTransformer = JsonTransformer(objectMapper)
     val taskRepository = TaskRepository()
-    val taskController = TaskController(taskRepository)
+    val taskController = TaskController(objectMapper, taskRepository)
 
     get("/tasks", taskController.index(), jsonTransformer)
+    post("/tasks", taskController.create(), jsonTransformer)
 }
--- src/main/kotlin/todolist/controllers/task_controller.kt
+++ src/main/kotlin/todolist/controllers/task_controller.kt
@@ -1,10 +1,28 @@
 package todolist.controllers
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import spark.Route
+import spark.Spark.halt
+import todolist.TaskCreateRequest
 import todolist.repositories.TaskRepository
 
-class TaskController(private val taskRepository: TaskRepository) {
+class TaskController(
+    private val objectMapper: ObjectMapper,
+    private val taskRepository: TaskRepository
+) {
     fun index(): Route = Route { req, res ->
         taskRepository.findAll()
+    }
+
+    fun create(): Route = Route { req, res ->
+        val request: TaskCreateRequest =
+            try {
+                objectMapper.readValue(req.bodyAsBytes(), TaskCreateRequest::class.java)
+            } catch (e: Exception) {
+                throw halt(400)
+            }
+        val task = taskRepository.create(request.content)
+        res.status(201)
+        task
     }
 }
